542323539a5f8f84184f8b60dc6d54ba1a9780af
Darko Damjanovic <darkodamjanovic@me.com>
2018-09-07 21:18:30 +0200

    Created Xcode project

---
+++ SolidRockAppTemplate/SolidRockAppTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolidRockAppTemplate
+//
+//  Created by Darko Damjanovic on 07.09.18.
+//  Copyright © 2018 SolidRock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
