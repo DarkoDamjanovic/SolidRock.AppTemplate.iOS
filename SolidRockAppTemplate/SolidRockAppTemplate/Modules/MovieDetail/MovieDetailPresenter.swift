22123ab3277977ac1e1464a0abdaa5c63dc2c708
Darko Damjanovic <darkodamjanovic@me.com>
2018-09-09 15:57:46 +0200

    Loading MovieDetail into UI

--- SolidRockAppTemplate/SolidRockAppTemplate/Modules/MovieDetail/MovieDetailPresenter.swift
+++ SolidRockAppTemplate/SolidRockAppTemplate/Modules/MovieDetail/MovieDetailPresenter.swift
@@ -10,6 +10,14 @@ import Foundation
 
 protocol MovieDetailPresenterProtocol {
     var viewTitle: String { get }
+    var title: String? { get }
+    var year: String? { get }
+    var director: String? { get }
+    var genre: String? { get }
+    var actors: String? { get }
+    var plot: String? { get }
+    var imagePosterURL: URL? { get }
+    func viewDidAppear()
 }
 
 /// This is just a template which can be re-used to create other modules.
@@ -19,6 +27,8 @@ class MovieDetailPresenter {
     private unowned let view: MovieDetailViewProtocol
     private let router: MovieDetailRouterProtocol
     private let imdbID: String
+    private let movieService: MovieServiceProtocol
+    private var movieDetail: MovieDetail?
     
     init(view: MovieDetailViewProtocol,
          router: MovieDetailRouterProtocol,
@@ -27,6 +37,7 @@ class MovieDetailPresenter {
         self.view = view
         self.router = router
         self.imdbID = imdbID
+        self.movieService = movieService
     }
     
     deinit {
@@ -37,6 +48,48 @@ class MovieDetailPresenter {
 extension MovieDetailPresenter: MovieDetailPresenterProtocol {
     var viewTitle: String {
         return "moviedetail.title".localized
+    }
+    
+    var title: String? {
+        return movieDetail?.title
+    }
+    
+    var year: String? {
+        return movieDetail?.year
+    }
+    
+    var director: String? {
+        return movieDetail?.director
+    }
+    
+    var genre: String? {
+        return movieDetail?.genre
+    }
+    
+    var actors: String? {
+        return movieDetail?.actors
+    }
+    
+    var plot: String? {
+        return movieDetail?.plot
+    }
+    
+    var imagePosterURL: URL? {
+        return movieDetail?.image
+    }
+    
+    func viewDidAppear() {
+        movieService.getMovieDetail(imdbID: imdbID) { [weak self] (result) in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let movieDetail):
+                strongSelf.movieDetail = movieDetail
+                strongSelf.view.loadUI()
+            case .failure(let error):
+                // TODO: error handling
+                strongSelf.log.error(error)
+            }
+        }
     }
 }
 
--- SolidRockAppTemplate/SolidRockAppTemplate/Modules/MovieDetail/MovieDetailViewController.swift
+++ SolidRockAppTemplate/SolidRockAppTemplate/Modules/MovieDetail/MovieDetailViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import SDWebImage
 
 protocol MovieDetailViewProtocol: class {
-
+    func loadUI()
 }
 
 class MovieDetailViewController: BaseViewController {
+    
+    @IBOutlet weak var imageViewPoster: UIImageView!
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelYear: UILabel!
+    @IBOutlet weak var labelDirector: UILabel!
+    @IBOutlet weak var labelGenre: UILabel!
+    @IBOutlet weak var labelActors: UILabel!
+    @IBOutlet weak var labelPlot: UILabel!
     
     var presenter: MovieDetailPresenterProtocol!
     
@@ -20,8 +29,21 @@ class MovieDetailViewController: BaseViewController {
         super.viewDidLoad()
         self.title = presenter.viewTitle
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        presenter.viewDidAppear()
+    }
 }
 
 extension MovieDetailViewController: MovieDetailViewProtocol {
-    
+    func loadUI() {
+        imageViewPoster.sd_setImage(with: presenter.imagePosterURL, completed: nil)
+        labelTitle.text = presenter.title
+        labelYear.text = presenter.year
+        labelGenre.text = presenter.genre
+        labelDirector.text = presenter.director
+        labelActors.text = presenter.actors
+        labelPlot.text = presenter.plot
+    }
 }
